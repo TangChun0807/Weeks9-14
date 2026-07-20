57cc328ed91139728363cce12b7c6d2737cca84e
CHOVYYUS_OMEN\tange <tangchun@sheridancollege.ca>
2026-07-20 13:45:38 -0400

    Finished challenge 2

--- Assets/Script/spritechanger.cs
+++ Assets/Script/spritechanger.cs
@@ -10,19 +10,38 @@ public class SpriteChanger : MonoBehaviour
 
     void Start()
     {
-        spriteRenderer.sprite = sprites[index];
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if (sprites.Length > 0)
+        {
+            spriteRenderer.sprite = sprites[index];
+        }
     }
 
     public void ChangeSprite(InputAction.CallbackContext context)
     {
-        if (!context.performed)
+        if (!context.started)
+        {
             return;
+        }
+
+        if (sprites.Length == 0)
+        {
+            return;
+        }
 
         index++;
 
         if (index >= sprites.Length)
+        {
             index = 0;
+        }
 
         spriteRenderer.sprite = sprites[index];
+
+        Debug.Log("Sprite changed to index: " + index);
     }
 }
